8553ae848b305bb30047531d220b0d4530e3cf5d
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-01 22:49:54 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+// TODO: Setup Redux
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build UBER!!!</Text>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
